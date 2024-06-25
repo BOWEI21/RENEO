54e15cd4c1662934efe4e58bc5979a4ba698ec68
BOWEI21 <167796095+BOWEI21@users.noreply.github.com>
2024-06-25 20:48:59 +0200

    Add files via upload

---
+++ Libft/libft.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bchen <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/25 20:38:15 by bchen             #+#    #+#             */
+/*   Updated: 2024/06/25 20:47:45 by bchen            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stddef.h>
+# include <stdio.h>
+# include <ctype.h>
+/*
+typedef struct	s_list
+{
+	void	*content
+	struct s_list	*next
+}t_list;
+*/
+
+#endif
